872b2659db639bc5cb4890afd73cd6380a307e86
Arison13 <arisonarias@gmail.com>
2022-06-01 00:12:19 -0400

    added js to make sticky nav, added styling, added services section but still need to add styling to that part, most of the project  is done already

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('connected')
+window.onscroll = () => {
+    SetSticky()
+}
+const navbar = document.getElementById("header");
+
+const sticky = navbar.offsetTop;
+
+function SetSticky() {
+  window.pageYOffset >= sticky ?  navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
